32e13ec0a9337c6b2eb7ff8797d1be576d7e8656
Michaela White <whitemic@sas.upenn.edu>
2018-06-14 10:15:39 -0400

    Added javascript file

---
+++ assets/main.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function() {
+    var startYear = 1900; //need these from front end
+    var endYear = 1920;
+    
+    
+    
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "762530d2f8954471933ab18746b3be13"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(results) {
+        for (var i = 0; i < 10; i++) { //variable for where we have the 5 value that comes from the input number for records to retreive 
+            //console.log(results.response.docs[i].headline.main);
+            var headline = results.response.docs[i].headline.main;
+            //this is how we get the title
+            var year = results.response.docs[i].pub_date;
+            var year1 = year.slice(0, 4);
+            //console.log(year);
+    
+            if (year1 > startYear && year1 < endYear) {
+                //we want these results
+                var articleDiv = $("<div>");
+                var p = $("<p>");
+                p.text(headline);
+                var date = $("<p>");
+                date.text(year);
+                articleDiv.append(p);
+                articleDiv.append(date);
+                $("#articles").append(articleDiv);
+            }
+        }
+    }).fail(function(err) {
+      throw err;
+    
+    
+    })
+});
